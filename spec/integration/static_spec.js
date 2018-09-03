6248c7964e4c5696b4666c0638042af99d2d4ed1
Brandon Kim <bkim2490@gmail.com>
2018-09-03 19:49:52 +0900

    intro-to-express-assignment: completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -15,4 +15,17 @@ describe('routes : static', () => {
       });
     });
   });
+
+  describe('GET /marco', () => {
+    it('should return status code 200 and body should be "polo"', (done) => {
+
+      request.get(base + 'marco', (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toBe('polo');
+
+        done();
+      });
+    });
+  });
+
 });
