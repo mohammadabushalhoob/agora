421743842645b4e64b86175ce558d518a49d08ce
Brandon Kim <bkim2490@gmail.com>
2018-09-18 15:04:08 +0900

    validations-checkpoint: completed

--- spec/integration/posts_spec.js
+++ spec/integration/posts_spec.js
@@ -71,6 +71,28 @@ describe('routes : posts', () => {
         }
       );
     });
+    it('should not create a new post that fails validations', (done) => {
+      const options = {
+        url: `${base}/${this.topic.id}/posts/create`,
+        form: {
+          title: 'a',
+          body: 'b'
+        }
+      };
+      request.post(options,
+        (err, res, body) => {
+          Post.findOne({where: {title: 'a'}})
+          .then((post) => {
+            expect(post).toBeNull();
+            done();
+          })
+          .catch((err) => {
+            console.log(err);
+            done();
+          });
+        }
+      );
+    })
   });
 
   describe('GET topics/:topicId/posts/:id', () => {
@@ -125,7 +147,9 @@ describe('routes : posts', () => {
       const options = {
         url: `${base}/${this.topic.id}/posts/${this.post.id}/update`,
         form: {
-          title: 'Snowman Building Competition'
+          title: 'Snowman Building Competition',
+          body: 'So much snow!',
+          topicId: this.topic.id
         }
       };
       request.post(options,
