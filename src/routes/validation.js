a4d6ed05a164bffc7e2fa8dc9b265a7c24a06d08
Brandon Kim <bkim2490@gmail.com>
2018-09-18 19:28:06 +0900

    authentication-checkpoint: completed

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -13,6 +13,7 @@ module.exports = {
       return next();
     }
   },
+
   validateTopics(req, res, next){
     if(req.method === 'POST'){
       req.checkBody('title', 'must be at least 5 characters in length').isLength({min: 5});
@@ -22,6 +23,21 @@ module.exports = {
     if(errors){
       req.flash('error', errors);
       return res.redirect(303, req.headers.referer);
+    } else {
+      return next();
+    }
+  },
+
+  validateUsers(req, res, next){
+    if(req.method === 'POST'){
+      req.checkBody('email', 'must be valid').isEmail();
+      req.checkBody('password', 'must be at least 6 characters in length').isLength({min: 6});
+      req.checkBody('passwordConfirmation', 'must match password provided').optional().matches(req.body.password);
+    }
+    const errors = req.validationErrors();
+    if(errors){
+      req.flash('error', errors);
+      return res.redirect(req.headers.referer);
     } else {
       return next();
     }
