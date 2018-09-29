e9c38337e0685cf5476342a5d3d11f79badcd331
Brandon Kim <bkim2490@gmail.com>
2018-09-29 20:57:33 +0900

    authorization-checkpoint: completed

---
+++ spec/unit/topic_spec.js
@@ -0,0 +1,73 @@
+const sequelize = require('../../src/db/models/index').sequelize;
+const Topic = require('../../src/db/models').Topic;
+const Post = require('../../src/db/models').Post;
+const User = require('../../src/db/models').User;
+
+describe('Topic', () => {
+
+    beforeEach((done) => {
+        this.topic;
+        this.post;
+        this.user;
+        sequelize.sync({force: true}).then((res) => {
+            User.create({
+                email: 'starman@tesla.com',
+                password: 'Trekkie4lyfe'
+            })
+            .then((user) => {
+                this.user = user;
+                Topic.create({
+                    title: 'Expeditions to Alpha Centauri',
+                    description: 'A compilation of reports from recent visits to the star system.',
+                    posts: [{
+                        title: 'My first visit to Proxima Centauri b',
+                        body: 'I saw some rocks',
+                        userId: this.user.id
+                    }]
+                }, {
+                    include: {
+                        model: Post,
+                        as: 'posts'
+                    }
+                })
+                .then((topic) => {
+                    this.topic = topic;
+                    this.post = topic.posts[0];
+                    done();
+                });
+            });
+        });
+    });
+
+    describe('#create()', () => {
+        it('should create a topic object with a title and description', (done) => {
+            Topic.create({
+                title: 'Hello',
+                description: 'There'
+            })
+            .then((topic) => {
+                expect(topic.title).toBe('Hello');
+                expect(topic.description).toBe('There');
+                done();
+            })
+            .catch((err) => {
+                console.log(err);
+                done();
+            });
+        });
+
+        it('should not create a topic with missing title or description', (done) => {
+            Topic.create({
+                title: 'Hello'
+            })
+            .then((topic) => {
+                done();
+            })
+            .catch((err) => {
+                expect(err.message).toContain('Topic.description cannot be null');
+                done();
+            });
+        });
+    });
+
+});
--- src/db/models/post.js
+++ src/db/models/post.js
@@ -2,24 +2,33 @@
 module.exports = (sequelize, DataTypes) => {
   var Post = sequelize.define('Post', {
     title: {
-     type: DataTypes.STRING,
-     allowNull: false
-   },
-   body: {
-     type: DataTypes.STRING,
-     allowNull: false
-   },
-   topicId: {
-     type: DataTypes.INTEGER,
-     allowNull: false
-   }
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  body: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  topicId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
   }, {});
   Post.associate = function(models) {
     // associations can be defined here
     Post.belongsTo(models.Topic, {
-       foreignKey: "topicId",
-       onDelete: "CASCADE"
-     });
+      foreignKey: "topicId",
+      onDelete: "CASCADE"
+    });
+
+    Post.belongsTo(models.User, {
+      foreignKey: 'userId',
+      onDelete: 'CASCADE'
+    });
   };
   return Post;
 };
